d30f6db098cf502236d6e421d26183db38481c90
o-masny <icelyy@seznam.cz>
2024-06-27 18:47:56 +0200

    fix: add dynamic import is maybe solution for this polution?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Footer from "@/lib/components/footer";
 import Navbar from "@/lib/components/navabr/navbar_body";
 import StickCursor from "@/lib/components/utils/cursor";
 import HomepageScreen from "@/lib/pages/menu_page";
+import dynamic from "next/dynamic";
 
 export default function Home() {
+  const HomepageScreen = dynamic(() => import("../lib/pages/menu_page"), {
+    ssr: false,
+  });
   return (
     <div>
       <StickCursor />
