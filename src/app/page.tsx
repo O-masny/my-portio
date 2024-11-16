a24ca062f14dcd301938007abb03998bf7cb8b60
ondrej masny <iicelyy@seznam.cz>
2024-11-16 09:55:36 +0100

    feat: remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,10 @@ import dynamic from "next/dynamic";
 import BurgerMenu from "@/lib/components/nav/burger_menu";
 import Footer from "@/lib/components/utils/components/footer";
 
-// Dynamicky importujeme WelcomePage, která je klientskou komponentou
 const WelcomePage = dynamic(() => import("../lib/pages/welcome_page"), {
   ssr: false,
 });
 
-// Dynamicky importujeme HomepageScreen, která je klientskou komponentou
 const HomepageScreen = dynamic(() => import("./home/page"), {
   ssr: false,
   loading: () => <WelcomePage />,
